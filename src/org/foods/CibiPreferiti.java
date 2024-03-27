98c6e3c4a9529b0e12b77cef9aca1168ea0df4fb
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-03-27 15:18:09 +0100

    add array with print

--- src/org/foods/CibiPreferiti.java
+++ src/org/foods/CibiPreferiti.java
@@ -1,7 +1,13 @@
 package org.foods;
 
+import java.util.Arrays;
+
 public class CibiPreferiti {
     public static void main(String[] args) {
-
+        String[] favouriteFoods = {"Cannelloni","Pizza","Timballo","Cozze","Hamburger","Sushi","Pinza"};
+        System.out.println("Nella classifica hai "+ favouriteFoods.length + " cibi preferiti");
+        System.out.println("Il tuo cibo preferito è : "+ favouriteFoods[0]);
+        System.out.println("Il cibo ultimo in classifica è : "+ favouriteFoods[favouriteFoods.length - 1]);
+        System.out.println("Il cibo a metà classifica : "+ favouriteFoods[favouriteFoods.length / 2]);
     }
 }
