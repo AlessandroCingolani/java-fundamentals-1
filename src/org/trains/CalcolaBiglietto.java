950481273f2fe6d6177d937b7d4920083e42a98b
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-03-27 14:34:02 +0100

    Change name class

--- src/org/trains/CalcolaBiglietto.java
+++ src/org/trains/CalcolaBiglietto.java
@@ -2,25 +2,27 @@ package org.trains;
 
 import java.util.Scanner;
 
-public class TrainTicket {
+public class CalcolaBiglietto {
     public static void main(String[] args) {
         //import scanner
         Scanner scan = new Scanner(System.in);
 
         //variables
         double priceKm = 0.21;
-        int underAgeDiscount = 20, over65Discount = 40;
+        int underAgeDiscount = 20, seniorDiscount = 40;
 
 
         // chiedi quanti chilometri deve percorrere
         System.out.print("How many kilometers you have to travel: ");
-        int km = scan.nextInt();
+        double km = scan.nextDouble();
         System.out.println(km);
 
         // chiedi l' età dell' utente
         System.out.print("How old are you: ");
         int age = scan.nextInt();
         System.out.println(age);
+
+
 
 
         scan.close();
